467c077183484407b62b8f20395e5a564b72deab
arweb1 <agaevrweb@gmail.com>
2023-08-18 13:42:07 +0300

    form done

--- src/components/heroesAddForm/HeroesAddForm.js
+++ src/components/heroesAddForm/HeroesAddForm.js
@@ -1,4 +1,5 @@
-
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 // Задача для этого компонента:
 // Реализовать создание нового героя с введенными данными. Он должен попадать
@@ -11,37 +12,65 @@
 // данных из фильтров
 
 const HeroesAddForm = () => {
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            text: '',
+            element: ''
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .min(2, "Name must be longer than 2 symbols")
+                .required('Name is Required'),
+            text: Yup.string()
+                .min(10, 'description must be longer than 10 symbols')
+                .required('Description is required'),
+            element: Yup.string().required('Required')
+        }),
+        onSubmit: values => console.log(JSON.stringify(values, null, 2))
+    })
+
     return (
-        <form className="border p-4 shadow-lg rounded">
+        <form className="border p-4 shadow-lg rounded" onSubmit={formik.handleSubmit}>
             <div className="mb-3">
                 <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
-                <input 
+                <input
                     required
-                    type="text" 
-                    name="name" 
-                    className="form-control" 
-                    id="name" 
-                    placeholder="Как меня зовут?"/>
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    id="name"
+                    placeholder="Как меня зовут?"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.name} />
             </div>
-
+            {formik.errors.name && formik.touched.name ? <div className="error">{formik.errors.name}</div> : null}
             <div className="mb-3">
                 <label htmlFor="text" className="form-label fs-4">Описание</label>
                 <textarea
                     required
-                    name="text" 
-                    className="form-control" 
-                    id="text" 
+                    name="text"
+                    className="form-control"
+                    id="text"
                     placeholder="Что я умею?"
-                    style={{"height": '130px'}}/>
+                    style={{ "height": '130px' }}
+                    value={formik.values.text}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                />
             </div>
-
+            {formik.errors.text && formik.touched.text ? <div className="error">{formik.errors.text}</div> : null}
             <div className="mb-3">
                 <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
-                <select 
+                <select
                     required
-                    className="form-select" 
-                    id="element" 
-                    name="element">
+                    className="form-select"
+                    id="element"
+                    name="element"
+                    value={formik.values.element}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}>
                     <option >Я владею элементом...</option>
                     <option value="fire">Огонь</option>
                     <option value="water">Вода</option>
@@ -49,7 +78,7 @@ const HeroesAddForm = () => {
                     <option value="earth">Земля</option>
                 </select>
             </div>
-
+            {formik.errors.element && formik.touched.element ? <div className="error">{formik.errors.element}</div> : null}
             <button type="submit" className="btn btn-primary">Создать</button>
         </form>
     )
